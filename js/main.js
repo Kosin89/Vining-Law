6a1b48e6b76b3fc80ea267b61f7d95b1a8058539
Kosin89 <110737422+Kosin89@users.noreply.github.com>
2023-10-15 09:39:44 +0200

    finished home

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const nav = document.querySelector(".navbar-collapse")
+const footerYear = document.querySelector(".footer__year")
+
 
 document.addEventListener("click", () => {
     if (nav.classList.contains("show")) {
         nav.classList.remove("show")
     }
 })
+
+
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
+
+
+
